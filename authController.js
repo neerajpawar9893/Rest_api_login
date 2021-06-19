680f5cae2fb77d2f4a32a318c61e3b47c7ffaf65
neerajpawar9893 <45584978+neerajpawar9893@users.noreply.github.com>
2021-06-19 13:47:53 +0530

    Add files via upload

---
+++ authController.js
@@ -0,0 +1,117 @@
+const User = require('../models/user');
+var jwt = require('jsonwebtoken');
+
+const bcrypt = require('bcrypt');
+
+const { validationResult } = require('express-validator/check');
+
+exports.signup = (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const error = new Error('Validation failed.');
+      error.statusCode = 422;
+      error.data = errors.array();
+      throw error;
+    }
+    const email = req.body.email;
+    const name = req.body.name;
+    const password = req.body.password;
+    bcrypt
+      .hash(password, 12)
+      .then(hashedPw => {
+        const user = new User({
+          email: email,
+          password: hashedPw,
+          name: name
+        });
+        return user.save();
+      })
+      .then(result => {
+        res.status(201).json({ message: 'User created!', userId: result._id });
+      })
+      .catch(err => {
+        if (!err.statusCode) {
+          err.statusCode = 500;
+        }
+        next(err);
+      });
+  };
+
+  exports.login =(req, res, next) => {
+    const email = req.body.email;
+    const password = req.body.password;
+    let loadedUser;
+
+    User.findOne({ email: email})
+    .then(user => {
+      if(!user){
+        const error = new Error('Wrong E-mail login failed');
+        error.statusCode = 401;
+        throw error;
+      }
+      loadedUser = user;
+      return bcrypt.compare(password, user.password);
+
+    })
+    .then(doMatch=> {
+      if(!doMatch){
+        const error = new Error('Wrong Password');
+        error.statusCode = 401;
+        throw error;
+      }
+      const token = jwt.sign({
+        email: loadedUser.email,
+        userId: loadedUser._id.toString()
+      },'somesecretkey',
+      {expiresIn: '1h'}
+      );
+      res.status(200).json({token: token, userId: loadedUser._id.toString() });
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode = 500;
+      }
+      next(err);
+    });
+  };
+
+  exports.getUserStatus = (req, res, next) =>  {
+    User.findById(req.userId)
+    .then(user => {
+      if(!user) {
+        const error = new Error('User not found');
+        error.statusCode= 404;
+        throw error;
+      }
+      res.status(200).json({status: user.status})
+    })
+    .catch(err => {
+      if(!err.statusCode){
+        err.statusCode = 500;
+      }
+      next(err);
+    })
+
+  };
+
+  exports.updateUserStatus = (req, res, next) => {
+    const newStatus = req.body.status;
+    User.findById(req.userId)
+    .then(user => {
+      if(!user) {
+        const error = new Error('User not found');
+        error.statusCode = 404;
+        throw error;
+      }
+      user.status = newStatus;
+      return user.save()
+    }).then(result => {
+      res.status(200).json({message: 'User updated'})
+    })
+    .catch(err => {
+      if(!err.statusCode) {
+        err.statusCode =500;
+      }
+      next(err);
+    })
+  };
